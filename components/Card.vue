5b1ad98f45e2a16cc8aba81ec60bd294fbab41f1
Daniil <dan-trofimov@mail.ru>
2021-05-30 01:20:34 +0300

    Set up donation hide-n-show

--- components/Card.vue
+++ components/Card.vue
@@ -16,9 +16,9 @@
         </p>
       </div>
       <div class="move-to-hide-show">
-        <span class="to-show">Не скрывать</span>
-        <span class="to-hide">Скрыть</span>
-        <!-- TODO make it work -->
+        <span :class="isHiddenClass">
+          {{ donation.is_hidden ? 'Не скрывать' : 'Скрыть' }}
+        </span>
       </div>
     </div>
   </div>
@@ -33,6 +33,14 @@ export default {
       default: () => {},
     },
   },
+  computed: {
+    isHiddenClass() {
+      return {
+        'to-show': this.donation.is_hidden,
+        'to-hide': !this.donation.is_hidden,
+      }
+    },
+  },
 }
 </script>
 
@@ -42,5 +50,60 @@ export default {
 }
 .admin-comment {
   text-align: center;
+}
+
+.donates-container .move-to-hide-show {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  display: inline-block;
+  padding: 16px;
+  background-color: white;
+  opacity: 0;
+  -webkit-transition: opacity 100ms;
+  -o-transition: opacity 100ms;
+  transition: opacity 100ms;
+}
+
+.donates-container:hover .move-to-hide-show {
+  opacity: 1;
+}
+
+.move-to-hide-show span {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+  color: #b73f3f;
+  width: 100%;
+  display: inline-block;
+  padding-left: 22px;
+  cursor: pointer;
+  box-sizing: border-box;
+}
+
+.move-to-hide-show:hover span {
+  color: #ff1f1f;
+}
+
+.to-hide {
+  background: url('@/assets/images/archive.svg') no-repeat center left
+    transparent;
+}
+
+.move-to-hide-show:hover .to-hide {
+  background: url('@/assets/images/archive-highlight.svg') no-repeat center left
+    transparent;
+}
+
+.to-show {
+  background: url('@/assets/images/unarchive.svg') no-repeat center left
+    transparent;
+}
+
+.move-to-hide-show:hover .to-show {
+  background: url('@/assets/images/unarchive-highlight.svg') no-repeat center
+    left transparent;
 }
 </style>
