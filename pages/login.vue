c41480881795ee5ddbcc1a549665f92ea7650edc
Daniil <dan-trofimov@mail.ru>
2021-06-07 02:05:10 +0300

    Login page WIP

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Here will be login form</div>
+  <div class="login-background">
+    <div class="login-form">
+      <el-form>
+        <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,3 +20,21 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.login-background {
+  text-align: center;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('@/assets/images/login-background.gif');
+}
+
+.login-form {
+  text-align: center;
+}
+</style>
